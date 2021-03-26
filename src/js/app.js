4e3f8c6db8a0c601f09356f5644478b9d0747c86
Christiano0407 <placebo0704@gmail.com>
2021-03-26 09:30:59 -0600

    slide-carousel

--- src/js/app.js
+++ src/js/app.js
@@ -2,17 +2,41 @@ let slidePosition = 0;
 const slides = document.getElementsByClassName(`carousel__item`);
 const totalSlides = slides.length;
 
+/*Muestra cuántos slides*/
 console.log(slides);
 
 /* Next */
 document.getElementById(`carousel__button--next`)
 .addEventListener("click", () => {
-	console.log("Hello!")
+	/* console.log("Hello!") */
+	moveToNextSlide()
 });
 
 /*previous*/
 document.getElementById(`carousel__button--prev`)
 .addEventListener("click", () => {
-	console.log("Hi")
+	/* console.log("Hi") */
+	moveToPrevSlide()
 });
+  
+    /*Actions and conditionals*/
+function updateSlidePosition()
+
+function moveToNextSlide () {
+	/* console.log("Next"); */
+	if(slidePosition === totalSlides -1) {
+		slidePosition = 0;
+	}else {
+		slidePosition++;
+	}
+}
+
+function moveToPrevSlide () {
+/* 	console.log("Previous"); */
+if(slidePosition === 0) {
+	slidePosition = 0;
+}else {
+	slidePosition++;
+}
+}
 
